0fb187a0b34b6515c6666f6edec96e73826924d7
unknown <james@balvedastudios.com>
2008-12-09 17:25:19 +0000

    Fixes alpha support and generally tidies stuff up, handles Rails paths properly now

--- lib/eve_corplogo_generator.rb
+++ lib/eve_corplogo_generator.rb
@@ -10,32 +10,33 @@ module Eve
     class Logo 
       attr_accessor :image, :layers, :output
       def initialize(shapes,colors,output_path=nil,fill=nil)
+        logger = Logger.new("log.txt")
+        logger.level = Logger::DEBUG
         @output = output_path
         @layers = []
         @colors = colors.reverse # Reverse because we're constructing back-first, whereas API provides front-first lists.
         @shapes = shapes.reverse # And shapes too...
         @shapes.each_with_index{|v,k|@layers.push(Layer.new(v,@colors[k])) if v > 0} # Build layers
-        @image = Magick::Image.new(64,64){self.format='PNG';if fill then self.background_color=fill;end} 
+        @image = Magick::Image.new(64,64){self.format='PNG';self.background_color='none';} 
         @layers.each do |l|
           @image = @image.composite(l.image,0,0,Magick::OverCompositeOp)
         end
         # Begin clever stuff to get transparency right.
         @image.view(0,0,64,64) do |v|
-          l1 = @layers[0].image.view(0,0,64,64) if @layers[0]
-          l2 = @layers[1].image.view(0,0,64,64) if @layers[1]
-          l3 = @layers[2].image.view(0,0,64,64) if @layers[2]
+          l1 = @layers[0].image.view(0,0,64,64) if @layers[0].image
+          l2 = @layers[1].image.view(0,0,64,64) if @layers[1].image
+          l3 = @layers[2].image.view(0,0,64,64) if @layers[2].image
           (0..63).each do |x|
             (0..63).each do |y|
-              r,g,b,a = v[x][y].red, v[x][y].green, v[x][y].blue, v[x][y].opacity
-              a1 = @layers[0] ? ((Magick::QuantumRange - l1[x][y].opacity) / Magick::QuantumRange) : Magick::QuantumRange
-              a2 = @layers[1] ? ((Magick::QuantumRange - l2[x][y].opacity) / Magick::QuantumRange) : Magick::QuantumRange
-              a3 = @layers[2] ? ((Magick::QuantumRange - l3[x][y].opacity) / Magick::QuantumRange) : Magick::QuantumRange
-              a = 1-(a1*a2*a3)
-              if a
-                v[x][y].red = (r/a).round
-                v[x][y].green = (g/a).round
-                v[x][y].blue = (b/a).round
-                v[x][y].opacity = (Magick::QuantumRange*a).round
+              a1 = defined?(l1) ? l1[x][y].opacity : 255
+              a2 = defined?(l2) ? l2[x][y].opacity : 255
+              a3 = defined?(l3) ? l3[x][y].opacity : 255
+              a = (a1*a2*a3)/16581375
+              if a > 0
+                v[x][y].red = (v[x][y].red/a).round
+                v[x][y].green = (v[x][y].green/a).round
+                v[x][y].blue = (v[x][y].blue/a).round
+                v[x][y].opacity = ((Magick::QuantumRange)*a).round
               end
             end
           end
@@ -51,14 +52,19 @@ module Eve
     # Each layer may carry a different colour, or no colour at all
     class Layer
       attr_accessor :image
-      def initialize(shape,color)
-        if shape > 0
-          shape_filename = "src/#{color}/#{shape}.png"
-          @image = Magick::Image::read(shape_filename).first
-        else
-          @image = Magick::Image.new(64,64){self.format='PNG'} # Blank image for a blank layer.
+      if defined?(RAILS_ROOT)
+        def initialize(shape,color)
+          if shape > 0
+            @image = Magick::Image::read("#{RAILS_ROOT}/vendor/plugins/eve_corplogo_generator/src/#{color}/#{shape}.png")[0]
+          end
+        end
+      else
+        def initialize(shape,color)
+          if shape > 0
+            @image = Magick::Image::read("src/#{color}/#{shape}.png")[0]
+          end
         end
       end
     end # Layer class
   end # CorporateLogo module
-end # Nexus module
+end # Eve module
